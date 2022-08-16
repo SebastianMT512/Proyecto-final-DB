25fb773fb5a4e76b029aabcbc8facb6e38eb7163
Sebastian Morales T <49050379+SebastianMT512@users.noreply.github.com>
2022-08-16 12:22:33 -0500

    Add files via upload

---
+++ Final.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE final;
+
+USE final;
+
+CREATE TABLE pais(id INT PRIMARY KEY auto_increment NOT NULL,
+ISO3 VARCHAR(4),
+nombre_pais VARCHAR(50) NOT NULL,
+ciudad VARCHAR(50),
+moneda_corriente VARCHAR(50));
+
+
+
+
